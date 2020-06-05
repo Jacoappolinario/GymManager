d1e13541dbfcae3570d53529fbfaf46bacdc9aa9
Jacó Apolinário <jacoapollinario@gmail.com>
2020-06-05 16:41:14 -0300

    add pagination

--- public/scripts.js
+++ public/scripts.js
@@ -7,37 +7,53 @@ for (item of menuItens) {
     }
 }
 
-// Paginação
-// totalPages = 20
-// selectedPage = 15
-// [1,...,13,14,15,16,17...,20]
+function paginate(selectedPage, totalPages) {
+    let pages = [],
+        oldPage
 
-let totalPages = 20,
-    selectedPage = 6,
-    pages = [],
-    oldPage
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
-for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesbeforeSelectedPage = currentPage >= selectedPage - 2
 
-    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
 
-    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        if(firstAndLastPage || pagesbeforeSelectedPage && pagesAfterSelectedPage) {
+            
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...")
+            }
 
-    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
-    
-    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
-        if (oldPage && currentPage - oldPage > 2) {
-            pages.push("...")
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
         }
+    }
+    return pages
+}
 
-        if (oldPage && currentPage - oldPage == 2) {
-            pages.push(oldPage + 1)
+const pagination = document.querySelector(".pagination")
+const filter = pagination.dataset.filter
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const pages = paginate(page, total)
+
+let elements = ""
+
+for (let page of pages) {
+    if(String(page).includes("...")) {
+        elements += `<span>${page}</span>`
+    } else {
+        if (filter) {
+            elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+        } else {
+            elements += `<a href="?page=${page}">${page}</a>`
         }
-
-        pages.push(currentPage)
-
-        oldPage = currentPage
     }
 }
 
-console.log(pages)
+pagination.innerHTML = elements
--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -11,24 +11,21 @@ module.exports = {
 
         const params = {
             filter,
+            page,
             limit,
             offset,
             callback(instructors) {
-                return res.render("instructors/index", { instructors, filter })
+
+                const pagination = {
+                    total: Math.ceil(instructors[0].total / limit),
+                    page
+                }
+
+                return res.render("instructors/index", { instructors, pagination, filter })
             }
         }
 
         Instructor.paginate(params)
-
-        // if (filter) {
-        //     Instructor.findBy(filter, function(instructors) {
-        //         return res.render("instructors/index", { instructors, filter })
-        //     })
-        // } else {
-        //     Instructor.all(function(instructors) {
-        //         return res.render("instructors/index", { instructors })
-        //     })
-        // }
 
     },
     create(req, res) {
